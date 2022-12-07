f861171bd2e99e0cd60cba5ae8c09c03fce19cb1
shashi <shashidhar1306@gmail.com>
2022-12-07 16:04:37 +0530

    first commit

---
+++ src/Test/Testbrowser2.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testbrowser2 {
+
+	
+	@Test
+	public void TestCheckgoogle() throws MalformedURLException
+	{
+		MutableCapabilities caps= new MutableCapabilities();
+		WebDriver driver= new RemoteWebDriver(  new URL("https:hub.browserstack.com/wd/hub"), caps);
+	
+		driver.get("https://rahulshettyacademy.com");
+		Assert.assertTrue(driver.getTitle().matches("Rahul Shetty Academy"));
+	}
+}
